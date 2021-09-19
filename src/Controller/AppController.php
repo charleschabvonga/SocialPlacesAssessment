7ac65ab421b76bc5c2d3f5e359b36e5ddea586f4
charleschabvonga <charleschabvonga@gmail.com>
2021-09-19 12:19:13 +0200

    Entry point clean up

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\SerializerInterface;
 
 class AppController extends AbstractController
 {
@@ -13,33 +12,24 @@ class AppController extends AbstractController
     #[Route('/', name: 'contact-us')]
     public function index(): Response
     {
-        return $this->render('base.html.twig', [
-            'controller_name' => 'AppController',
-        ]);
+        return $this->render('base.html.twig');
     }
 
     #[Route('/login', name: 'login')]
-    public function login(SerializerInterface $serializer)
+    public function login(): Response
     {
-        return $this->render('base.html.twig', [
-            'user' => $serializer->serialize($this->getUser(), 'jsonld')
-        ]);
+        return $this->render('base.html.twig');
     }
 
     #[Route('/register', name: 'register')]
     public function register(): Response
     {
-        return $this->render('base.html.twig', [
-            'controller_name' => 'AppController',
-        ]);
+        return $this->render('base.html.twig');
     }
 
     #[Route('/messages', name: 'messages')]
-    public function messages(SerializerInterface $serializer)
+    public function messages(): Response
     {
-        return $this->render('base.html.twig', [
-            'user' => $serializer->serialize($this->getUser(), 'jsonld')
-        ]);
+        return $this->render('base.html.twig');
     }
-
 }
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,29 +3,18 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-// use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
-// #[ApiResource]
-// #[Route('/api')]
 class SecurityController extends AbstractController
 {
     #[Route('/login', name: 'app_login', methods: 'POST')]
-    public function login(Request $request): JsonResponse
+    public function login(Request $request): Response
     {
         $user = $this->getUser();
 
-        // return $this->json([
-        //     // The getUserIdentifier() method was introduced in Symfony 5.3.
-        //     // In previous versions it was called getUsername()
-        //     'username' => $user->getUserIdentifier(),
-        //     'roles' => $user->getRoles(),
-        // ]);
-
-        return new JsonResponse(null, 204, [
+        return $this->json([
             'username' => $user->getUserIdentifier(),
             'roles' => $user->getRoles(),
         ]);
